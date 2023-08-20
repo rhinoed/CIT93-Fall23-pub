34e5a8591f00bd347d8fff8e17e8b598df712a28
Mark Edmunds <edmundsmd1@gmail.com>
2023-08-19 22:35:32 -0700

    setup done

---
+++ w3/main.js
@@ -0,0 +1,31 @@
+/*
+CIT 93
+Mark Edmunds
+August 19, 2023  
+w3 CF Household and Functions
+ */
+
+// setup code from marke-pri w3
+let carbonFooprintPoints = 0;
+const numberInHoushold = 3;
+
+if (numberInHoushold === 1){
+    carbonFooprintPoints = carbonFooprintPoints + 14;
+}else if (numberInHoushold === 2){
+    carbonFooprintPoints = carbonFooprintPoints + 12;
+}else if (numberInHoushold === 3){
+    carbonFooprintPoints = carbonFooprintPoints + 10;
+}else if (numberInHoushold === 4){
+    carbonFooprintPoints = carbonFooprintPoints + 8;
+}else if (numberInHoushold === 5){
+    carbonFooprintPoints = carbonFooprintPoints + 6;
+}else if (numberInHoushold === 6){
+    carbonFooprintPoints = carbonFooprintPoints + 4;
+}else if (numberInHoushold > 6){
+    carbonFooprintPoints = carbonFooprintPoints + 2;
+}else{
+    // only condition left is household is zero
+    console.log("no update to points household is empty");
+}
+
+console.log(`Given the number in household ${numberInHoushold} the carbon footprint score is ${carbonFooprintPoints}`);
